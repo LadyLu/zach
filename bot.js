784066e9d1ccedcafb98dbb9a5b471dd1c1a7bc4
LadyLu <41097277+LadyLu@users.noreply.github.com>
2018-07-12 15:53:54 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+  if (message.content.includes("a")) {
+  message.channel.send("REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+  } else
+  
+  if (message.content.includes("e")) {
+  message.channel.send("REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+  } else
+  
+  if (message.content.includes("o")) {
+  message.channel.send("REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+  } else
+  
+  if (message.content.includes("i")) {
+  message.channel.send("REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+  }
+  
+ 
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
