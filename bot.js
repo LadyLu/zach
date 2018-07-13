2002dfdf02b58ecd96e57210aa437b3c327a190b
LadyLu <41097277+LadyLu@users.noreply.github.com>
2018-07-12 22:50:09 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,21 +6,33 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  if (message.content.includes("a")) {
-  message.channel.send("REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+  if (message.content.includes("Leo")) {
+  message.channel.send("He may be fast, but Oi'm much  stronger than that kitty.");
   } else
   
-  if (message.content.includes("e")) {
-  message.channel.send("REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+  if (message.content.includes("Venom")) {
+  message.channel.send("Piece of Piss.");
   } else
   
-  if (message.content.includes("o")) {
-  message.channel.send("REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+  if (message.content.includes("Monique")) {
+  message.channel.send("Can we not mention her....?");
   } else
   
-  if (message.content.includes("i")) {
-  message.channel.send("REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
- }
+  if (message.content.includes("Valentina")) {
+  message.channel.send("She's a pretty sheila she is.");
+  } else
+     
+  if (message.content.includes("Am I pretty Zach")) {
+  message.channel.send("GO ASK THE MIRROR!");
+  } else
+      
+  if (message.content.includes("Serpintine")) {
+  message.channel.send("AH-OI GOTTA GO, MATE.");
+  }
+    
+  if (message.content.includes("I love you Zach")) {
+  message.channel.send("Bloody hell, I love ya too.");
+  }
 
  
 });
